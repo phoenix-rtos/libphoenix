c6b5880bbd664602ca097dfcdf90610150e712ec
Andrzej Asztemborski <andrzej.asztemborski@phoesys.com>
2017-08-10 18:29:17 +0200

    Update message header according to flash driver reqirements
    Flash driver needs msgdata_t type, so direct port of kernel recv was
    added for consistency.

--- sys/msg.h
+++ sys/msg.h
@@ -29,10 +29,25 @@ typedef enum _msgop_t {
 } msgop_t;
 
 
+typedef struct _msghdr_t {
+	msgtype_t type;
+	msgop_t op;
+	size_t rsize;
+	unsigned int sender;
+} msghdr_t;
+
+
+typedef struct {
+	size_t offset;
+	void *buff;
+	size_t size;
+} msgdata_t;
+
+
 extern int send(u32 port, msgop_t op, void *data, size_t size, msgtype_t type, void *rdata, size_t rsize);
 
 
-extern int recv(u32 port, void *data, size_t size, msgtype_t *type, msgop_t *op, size_t *rsize, unsigned int *sender);
+extern int recv(u32 port, void *data, size_t size, msghdr_t *hdr);
 
 
 extern int respond(u32 port, int err, void *data, size_t size);
