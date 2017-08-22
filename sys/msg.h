ed28a73de6b282d98b757361c884210fc0a73ad7
Aleksander Kaminski <aleksander.kaminski@atendesoftware.pl>
2017-08-22 11:56:36 +0200

    Added lookup syscall

--- sys/msg.h
+++ sys/msg.h
@@ -53,4 +53,7 @@ extern int recv(u32 port, void *data, size_t size, msghdr_t *hdr);
 extern int respond(u32 port, int err, void *data, size_t size);
 
 
+extern int lookup(char *name, unsigned int *port);
+
+
 #endif
