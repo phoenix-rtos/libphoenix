57cff200971106a68ddfb1d3e2602a77633a1eec
Aleksander Kaminski <aleksander.kaminski@atendesoftware.pl>
2017-08-22 15:57:43 +0200

    Added keepidle syscall

---
+++ sys/pwman.h
@@ -0,0 +1,25 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * sys/pwman
+ *
+ * Copyright 2017 Phoenix Systems
+ * Author: Aleksander Kaminski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_SYS_PWMAN_H_
+#define _LIBPHOENIX_SYS_PWMAN_H_
+
+#include ARCH
+
+
+extern void keepidle(int s);
+
+
+#endif
