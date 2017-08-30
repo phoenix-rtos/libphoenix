22ae9e34ab4f3f037740f71fa0c47d1b0096ca4a
Andrzej Asztemborski <andrzej.asztemborski@phoesys.com>
2017-08-30 16:18:52 +0200

    Added boolean definition header
    stdbool.h is needed by meterapp.

---
+++ stdbool.h
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * stdbool.h
+ *
+ * Copyright 2017 Phoenix Systems
+ * Author: Andrzej Asztemborski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_STDBOOL_H_
+#define _LIBPHOENIX_STDBOOL_H_
+
+
+typedef _Bool bool;
+enum { false, true };
+
+
+#endif
