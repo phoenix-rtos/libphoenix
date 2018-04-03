72b0149db27d6534f71934d2bd242bc226acbf99
Michał Mirosław <michal.miroslaw@phoenix-rtos.com>
2018-04-03 17:07:31 +0200

    stdio: asprintf()

---
+++ stdio/asprintf.c
@@ -0,0 +1,38 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * vsprintf.c
+ *
+ * Copyright 2017 Phoenix Systems
+ * Author: Adrian Kepka
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include "stdlib.h"
+#include "stdio.h"
+
+
+int vasprintf(char **strp, const char *fmt, va_list ap)
+{
+	*strp = malloc(1024); /* FIXME */
+	vsprintf(*strp, fmt, ap);
+	return 0;
+}
+
+
+int asprintf(char **strp, const char *format, ...)
+{
+	va_list ap;
+	int retVal;
+
+	va_start(ap, format);
+	retVal = vasprintf(strp, format, ap);
+	va_end(ap);
+
+	return retVal;
+}
