655e151ac121552177aecf9ac0d1e0f1d899ac0e
Krystian Wasik <krystian.wasik@phoesys.com>
2018-04-11 15:22:38 +0200

    unistd: Implemented getopt

--- unistd/getopt.c
+++ unistd/getopt.c
@@ -6,7 +6,7 @@
  * unistd getopt
  *
  * Copyright 2018 Phoenix Systems
- * Author: Jan Sikorski
+ * Author: Jan Sikorski, Krystian Wasik
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -16,25 +16,76 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
+#include <stdio.h>
 
 
 char *optarg;
 
 
-int optind = 1, opterr, optopt;
+int optind = 1, opterr = 1, optopt;
+
+
+static int optwhere = 1;
 
 
 int getopt(int argc, char * const argv[], const char *optstring)
 {
+	char opt;
+	char *optspec;
+	int leading_colon;
+
 	if (argv[optind] == NULL || *argv[optind] != '-' || !strcmp(argv[optind], "-"))
 		return -1;
 
-	if (!strcmp(argv[optind], "--")) {
+	if (argv[optind][1] == '-') {
 		optind++;
 		return -1;
 	}
 
-	return -1;
+	opt = argv[optind][optwhere++];
+	leading_colon = *optstring == ':';
+
+	optspec = strchr(optstring, opt);
+	if (optspec == NULL) { /* Unrecognized option */
+		optopt = opt;
+		if (opterr && !leading_colon)
+			fprintf(stderr, "%s: illegal option -- %c\n", argv[0], opt);
+		if (argv[optind][optwhere] == '\0') {
+			optind++;
+			optwhere = 1;
+		}
+		return '?';
+	}
+
+	if (*(++optspec) == ':') {
+		if (argv[optind][optwhere] != '\0') { /* Argument in the same argv */
+			optarg = &argv[optind][optwhere];
+
+		} else if (optind + 1 < argc) { /* Argument in the next argv */
+			optarg = argv[optind + 1];
+			optind ++;
+
+		} else { /* Argument not provided */
+			optopt = opt;
+			optind++;
+			optwhere = 1;
+			if (leading_colon) {
+				return ':';
+			} else {
+				if (opterr)
+					fprintf(stderr, "%s: option requires an argument -- %c\n", argv[0], opt);
+				return '?';
+			}
+		}
+
+	} else if (argv[optind][optwhere] != '\0') { /* More options in the same argv */
+		return opt;
+	}
+
+	optwhere = 1;
+	optind++;
+
+	return opt;
 }
 
 
