d6888defbcc3336948c681cfcf81d7108fc467c0
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2018-06-05 15:25:41 +0200

    stdfile: set file descriptors for std{in,out,err}

--- stdio/stdfile.c
+++ stdio/stdfile.c
@@ -16,5 +16,8 @@
 #include <stdio.h>
 
 
-static FILE stdin_file, stdout_file;
-FILE *stdin = &stdin_file, *stdout = &stdout_file, *stderr = &stdout_file;
+static FILE stdin_file  = {0, NULL, 0};
+static FILE stdout_file = {1, NULL, 0};
+static FILE stderr_file = {2, NULL, 0};
+
+FILE *stdin = &stdin_file, *stdout = &stdout_file, *stderr = &stderr_file;
