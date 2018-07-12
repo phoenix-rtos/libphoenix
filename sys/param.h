d3beef8a46628e80387b953eea2ff1d1b5dd4c65
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2018-07-12 11:31:46 +0200

    Filled out sys/param.h

--- sys/param.h
+++ sys/param.h
@@ -1,1 +1,23 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * sys/param.h
+ *
+ * Copyright 2018 Phoenix Systems
+ * Author: Jan Sikorski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _SYS_PARAM_H_
+#define _SYS_PARAM_H_
+
+#include <sys/types.h>
+
 #define MAXPATHLEN 1024
+
+#endif
