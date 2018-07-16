2eb121d4864a6c89ad6513fefcb84e147fcbadc9
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2018-07-16 10:33:47 +0200

    Added sys/reboot.h

---
+++ sys/reboot.h
@@ -0,0 +1,26 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * sys/reboot.h
+ *
+ * Copyright 2018 Phoenix Systems
+ * Author: Jan Sikorski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_SYS_REBOOT_H_
+#define _LIBPHOENIX_SYS_REBOOT_H_
+
+#define RB_HALT_SYSTEM 0
+#define RB_AUTOBOOT 1
+
+
+extern int reboot(int magic);
+
+
+#endif
