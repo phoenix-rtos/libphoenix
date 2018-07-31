72dea95821e52297da87c87403a553ca3c93c15d
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2018-07-31 18:38:20 +0200

    termios.c: Fixed coding style

--- termios/termios.c
+++ termios/termios.c
@@ -33,18 +33,18 @@ int tcsetattr(int fildes, int optional_actions, const struct termios *termios_p)
 {
 	int cmd;
 	switch (optional_actions) {
-	case TCSANOW:
-		cmd = TCSETS;
-		break;
-	case TCSADRAIN:
-		cmd = TCSETSW;
-		break;
-	case TCSAFLUSH:
-		cmd = TCSETSF;
-		break;
-	default:
-		errno = EINVAL;
-		return -1;
+		case TCSANOW:
+			cmd = TCSETS;
+			break;
+		case TCSADRAIN:
+			cmd = TCSETSW;
+			break;
+		case TCSAFLUSH:
+			cmd = TCSETSF;
+			break;
+		default:
+			errno = EINVAL;
+			return -1;
 	}
 
 	int ret = ioctl(fildes, cmd, termios_p);
@@ -58,56 +58,56 @@ int tcsetattr(int fildes, int optional_actions, const struct termios *termios_p)
 
 int tcsendbreak(int fd, int duration)
 {
-        int ret = ioctl(fd, TCSBRK, duration);
-        if (ret < 0) {
-                errno = -ret;
-                return -1;
-        }
+	int ret = ioctl(fd, TCSBRK, duration);
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
 
-        return ret;
+	return ret;
 }
 
 int tcflush(int fd, int queue_selector)
 {
-        int ret = ioctl(fd, TCFLSH, queue_selector);
-        if (ret < 0) {
-                errno = -ret;
-                return -1;
-        }
+	int ret = ioctl(fd, TCFLSH, queue_selector);
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
 
-        return ret;
+	return ret;
 }
 
 int tcdrain(int fd)
 {
-        int ret = ioctl(fd, TCDRAIN, 0);
-        if (ret < 0) {
-                errno = -ret;
-                return -1;
-        }
+	int ret = ioctl(fd, TCDRAIN, 0);
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
 
-        return ret;
+	return ret;
 }
 
 int tcsetpgrp(int fd, pid_t pgrp)
 {
-        int ret = ioctl(fd, TIOCSPGRP, &pgrp);
-        if (ret < 0) {
-                errno = -ret;
-                return -1;
-        }
+	int ret = ioctl(fd, TIOCSPGRP, &pgrp);
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
 
-        return ret;
+	return ret;
 }
 
 pid_t tcgetpgrp(int fd)
 {
-        pid_t p;
-        int ret = ioctl(fd, TIOCGPGRP, &p);
-        if (ret < 0) {
-                errno = -ret;
-                return (pid_t)-1;
-        }
+	pid_t p;
+	int ret = ioctl(fd, TIOCGPGRP, &p);
+	if (ret < 0) {
+		errno = -ret;
+		return (pid_t)-1;
+	}
 
-        return p;
+	return p;
 }
