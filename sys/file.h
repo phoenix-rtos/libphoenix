8a419d03d437da8df0345fab1ccc0a1476e1ca09
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2018-08-13 17:59:48 +0200

    Added atEventMask

--- sys/file.h
+++ sys/file.h
@@ -22,7 +22,7 @@
 enum { otDir = 0, otFile, otDev, otSymlink, otUnknown };
 
 
-enum { atMode = 0, atUid, atGid, atSize, atType, atPort, atPollStatus, atCTime, atMTime, atATime, atLinks, atDev };
+enum { atMode = 0, atUid, atGid, atSize, atType, atPort, atPollStatus, atEventMask, atCTime, atMTime, atATime, atLinks, atDev };
 
 
 extern int fileAdd(unsigned int *h, oid_t *oid, unsigned mode);
