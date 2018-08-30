0bd60bda179b378aea5207e1da526886f0d15927
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2018-08-30 16:14:01 +0200

    strtoull: fix setting endptr in case of invalid hex string

--- stdlib/strtoull.c
+++ stdlib/strtoull.c
@@ -39,17 +39,19 @@ unsigned long long int strtoull(const char *nptr, char **endptr, int base)
 			c = *s++;
 	}
 
+	any = 0;
+	acc = 0;
+
 	if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
 		c = s[1];
 		s += 2;
 		base = 16;
+		any = 1;
 	}
 
 	if (base == 0)
 		base = c == '0' ? 8 : 10;
 
-	acc = 0;
-	any = 0;
 
 	if (base < 2 || base > 36)
 		return acc;
