a9fd54787dc118e35cbc395a4c9e72a4d2efad76
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2018-09-06 16:34:36 +0200

    types: add BSD legacy types for compatibility

--- sys/types.h
+++ sys/types.h
@@ -45,4 +45,11 @@ typedef unsigned char u_char;
 typedef unsigned short u_short;
 typedef unsigned int u_int;
 
+/* BSD legacy types permitted by POSIX */
+typedef uint8_t   u_int8_t;
+typedef uint16_t  u_int16_t;
+typedef uint32_t  u_int32_t;
+typedef uint64_t  u_int64_t;
+typedef int register_t;
+
 #endif
