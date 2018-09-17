02e16e89b97f6d9852b2e239476421d587bef0ab
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2018-09-17 16:17:28 +0200

    termios: added cfmakeraw

--- termios/termios.c
+++ termios/termios.c
@@ -123,3 +123,15 @@ pid_t tcgetsid(int fd)
 
 	return p;
 }
+
+void cfmakeraw(struct termios *termios_p)
+{
+	termios_p->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	termios_p->c_oflag &= ~OPOST;
+	termios_p->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	termios_p->c_cflag &= ~(CSIZE | PARENB);
+	termios_p->c_cflag |= CS8;
+
+	termios_p->c_cc[VTIME] = 0;
+	termios_p->c_cc[VMIN] = 1;
+}
