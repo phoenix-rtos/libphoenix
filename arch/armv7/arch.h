7b54551ebee4cd6f5744a13df3f69ecab75fbae7
Igor Gorniak <igor.gorniak@phoenix-rtos.com>
2018-10-27 11:19:41 +0200

    arch: add arch-dependent printf specifiers

--- arch/armv7/arch.h
+++ arch/armv7/arch.h
@@ -84,4 +84,32 @@ typedef u32 handle_t;
 
 #define SIZE_PAGE 0x200
 
+#define PRId8       "d"
+#define PRIu8       "u"
+#define PRIx8       "x"
+#define PRIX8       "X"
+#define PRIi8       "i"
+#define PRIo8       "o"
+
+#define PRId16      "d"
+#define PRIu16      "u"
+#define PRIx16      "x"
+#define PRIX16      "X"
+#define PRIi16      "i"
+#define PRIo16      "o"
+
+#define PRId32      "d"
+#define PRIu32      "u"
+#define PRIx32      "x"
+#define PRIX32      "X"
+#define PRIi32      "i"
+#define PRIo32      "o"
+
+#define PRId64      "lld"
+#define PRIu64      "llu"
+#define PRIx64      "llx"
+#define PRIX64      "llX"
+#define PRIi64      "lli"
+#define PRIo64      "llo"
+
 #endif
