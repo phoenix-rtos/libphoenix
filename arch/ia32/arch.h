bbb0a864def84f385ed8b606acac8ba0409a7ec5
Michał Mirosław <michal.miroslaw@phoenix-rtos.com>
2018-11-26 18:24:18 +0100

    arch: fix s8 signedness

--- arch/ia32/arch.h
+++ arch/ia32/arch.h
@@ -45,7 +45,7 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
 
-typedef char s8;
+typedef signed char s8;
 typedef short s16;
 typedef int s32;
 typedef long long s64;
