521189ac2bc9dedbc96d6f3555348c75f32e8be2
Kamil Amanowicz <kamil.amanowicz@phoenix-rtos.com>
2018-12-07 14:13:15 +0100

    sys/types: added u_long typedef

--- sys/types.h
+++ sys/types.h
@@ -44,6 +44,7 @@ typedef struct _FILE FILE;
 typedef unsigned char u_char;
 typedef unsigned short u_short;
 typedef unsigned int u_int;
+typedef unsigned long u_long;
 
 /* BSD legacy types permitted by POSIX */
 typedef uint8_t   u_int8_t;
