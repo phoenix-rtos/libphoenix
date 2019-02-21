12c5773d9ed38408a83c92871bd60983f35b91d9
Kamil Amanowicz <kamil.amanowicz@phoenix-rtos.com>
2019-02-21 09:08:04 +0100

    net: added inet_ntop() and inet_pton() stubs

---
+++ net/inet_ntop.c
@@ -0,0 +1,7 @@
+#include <arpa/inet.h>
+
+const char *inet_ntop(int af, const void *restrict src,
+		       char *restrict dst, socklen_t size) 
+{
+	return NULL;
+}
---
+++ net/inet_pton.c
@@ -0,0 +1,6 @@
+#include <arpa/inet.h>
+
+int inet_pton(int af, const char *restrict src, void *restrict dst)
+{
+	return 0;
+}
