a62121694b8ade794ef71273a68442a25fce0286
Pawel Pisarczyk <pawel.pisarczyk@phoesys.com>
2019-02-24 08:55:15 +0100

    Adding building scripts

---
+++ build-core.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# Shell script for building Phoenix-RTOS firmware
+#
+# Builder for Phoenix-RTOS core components
+#
+# Copyright 2018, 2019 Phoenix Systems
+# Author: Kaja Swat, Aleksander Kaminski, Pawel Pisarczyk
+#
+
+b_log "Building phoenix-rtos-kernel"
+
+KERNEL_MAKECMDGOALS=""
+if [ "X$CLEAN" == "Xclean" ]; then
+	KERNEL_MAKECMDGOALS="$CLEAN install-headers"
+fi
+
+(cd phoenix-rtos-kernel/src/ && make $MAKEFLAGS $KERNEL_MAKECMDGOALS all)
+cp -a phoenix-rtos-kernel/phoenix-arm-imx.elf _build
+cp -a phoenix-rtos-kernel/phoenix-arm-imx.img _build
+
+b_log "Building libphoenix"
+(cd libphoenix && make $MAKEFLAGS $CLEAN all install)
+b_install $PREFIX_PROG_STRIPPED/psh /bin
+
+b_log "Building phoenix-rtos-filesystems"
+(cd phoenix-rtos-filesystems && make $MAKEFLAGS $CLEAN all)
+b_install "$PREFIX_PROG_STRIPPED/dummyfs" /sbin
+
+b_log "Building phoenix-rtos-devices"
+(cd phoenix-rtos-devices && make $MAKEFLAGS $CLEAN all)
+b_install "$PREFIX_PROG_STRIPPED/imx6ull-gpio" /sbin
+b_install "$PREFIX_PROG_STRIPPED/imx6ull-uart" /sbin
+b_install "$PREFIX_PROG_STRIPPED/imx6ull-sdma" /sbin
+b_install "$PREFIX_PROG_STRIPPED/imx6ull-nandtool" /sbin
+
+b_log "Building phoenix-rtos-usb"
+(cd phoenix-rtos-usb && make $MAKEFLAGS $CLEAN all)
+b_install "$PREFIX_PROG_STRIPPED/usb" /sbin
+
+b_log "Building USB device drivers"
+(cd phoenix-rtos-devices && make $MAKEFLAGS $CLEAN all gsm/telit-le910)
+b_install "$PREFIX_PROG_STRIPPED/telit-le910" /sbin
+
+b_log "phoenix-rtos-lwip"
+(cd phoenix-rtos-lwip && make $MAKEFLAGS $CLEAN all)
+b_install "$PREFIX_PROG_STRIPPED/lwip" /sbin
+
+b_log "Building posixsrv"
+(cd phoenix-rtos-posixsrv && make $MAKEFLAGS $CLEAN all)
+b_install "$PREFIX_PROG_STRIPPED/posixsrv" /bin
+
+b_log "Building phoenixd"
+(cd phoenixd/ && make $MAKEFLAGS $CLEAN all)
+cp phoenixd/phoenixd $PREFIX_BOOT
---
+++ build.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# Shell script for building Phoenix-RTOS based firmware
+#
+# Main builder
+#
+# Copyright 2018, 2019 Phoenix Systems
+# Author: Kaja Swat, Aleksander Kaminski, Pawel Pisarczyk
+#
+
+set -e
+
+TARGET=arm-imx6ull
+TARGET_FAMILY=arm
+
+TOPDIR="$(pwd)"
+
+PREFIX_BUILD="$(pwd)/_build/$TARGET"
+PREFIX_FS=$(realpath "_fs")
+PREFIX_BOOT=$(realpath "_boot")
+
+PREFIX_PROG="$PREFIX_BUILD/prog/"
+PREFIX_PROG_STRIPPED="$PREFIX_BUILD/prog.stripped/"
+
+PREFIX_A="$PREFIX_BUILD/lib/"
+PREFIX_H="$PREFIX_BUILD/include/"
+
+CROSS=arm-phoenix-
+CFLAGS="${CFLAGS} -I${PREFIX_H}"
+LDFLAGS="$LDFLAGS -L$PREFIX_A"
+CC=${CROSS}gcc
+AS=${CROSS}as
+LD=${CROSS}ld
+AR=${CROSS}ar
+
+CLEAN="clean"
+#CLEAN=""
+MAKEFLAGS="--no-print-directory -j 19"
+
+export TARGET TARGET_FAMILY TOPDIR PREFIX_BUILD PREFIX_FS PREFIX_BOOT PREFIX_PROG PREFIX_PROG_STRIPPED PREFIX_A PREFIX_H CROSS CFLAGS LDFLAGS CC LD AR CLEAN MAKEFLAGS
+
+. ./phoenix-rtos-build/build.subr
+
+export BUSYBOX_CONFIG=""
+
+#
+# Preparing filesystem
+#
+b_log "Preparing filesystem"
+if [ "X$CLEAN" == "Xclean" ]; then
+	rm -fr $PREFIX_FS/root/*
+fi
+
+mkdir -p $PREFIX_FS/root
+cp -a $PREFIX_FS/root-skel/* $PREFIX_FS/root
+
+b_log "Saving git-version"
+echo " "$(git rev-parse HEAD)" incotex" > $PREFIX_FS/root/etc/git-version
+git submodule status >> $PREFIX_FS/root/etc/git-version
+
+mkdir -p $PREFIX_BUILD
+
+#
+# Compile and install components
+#
+./phoenix-rtos-build/build-core.sh
+./phoenix-rtos-ports/busybox/build.sh
+
+#./phoenix-ports/pcre/build.sh
+#./phoenix-ports/dropbear/build.sh
+#./phoenix-ports/lighttpd/build.sh
+
+exit 0
