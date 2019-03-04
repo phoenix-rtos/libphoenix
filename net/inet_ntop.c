8bac6c2af22ecfeeda88648287e876054908d386
Kamil Amanowicz <kamil.amanowicz@phoenix-rtos.com>
2019-03-04 15:26:21 +0100

    net: inet_ntop returns unspecified address for ipv6

--- net/inet_ntop.c
+++ net/inet_ntop.c
@@ -1,7 +1,13 @@
 #include <arpa/inet.h>
+#include <string.h>
 
 const char *inet_ntop(int af, const void *restrict src,
 		       char *restrict dst, socklen_t size) 
 {
+	if (af == AF_INET6) {
+		memset(dst, 0, size);
+		memcpy(dst, "::", strlen("::"));
+		return dst;
+	}
 	return NULL;
 }
