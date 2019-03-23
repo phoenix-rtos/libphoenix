c72a87d39401e77624e50cd13c93a6dd419de251
Michał Mirosław <michal.miroslaw@phoenix-rtos.com>
2019-03-23 17:36:20 +0100

    stdio: implement perror()

--- stdio/perror.c
+++ stdio/perror.c
@@ -5,14 +5,20 @@
  *
  * perror.c
  *
- * Copyright 2018 Phoenix Systems
- * Author: Kamil Amanowicz
+ * Copyright 2019 Phoenix Systems
+ * Author: Michał Mirosław
  *
  * This file is part of Phoenix-RTOS.
  *
  * %LICENSE%
  */
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
 void perror(const char *str)
 {
+	if (!str)
+		str = "";
+	fprintf(stderr, "%s%s%s\n", str, *str ? ": " : "", strerror(errno));
 }
