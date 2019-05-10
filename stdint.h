a599074e8781aa34ed1d54bdcb79464ddcb2bba6
Krystian Wasik <krystian.wasik@phoenix-rtos.com>
2019-05-10 17:27:52 +0200

    Added missing limit definitions to stdint.h

--- stdint.h
+++ stdint.h
@@ -32,6 +32,10 @@ typedef s64 int64_t;
 typedef uint64_t uintmax_t;
 typedef int64_t intmax_t;
 
+#ifndef SIZE_MAX
+#define SIZE_MAX (~(size_t)0)
+#endif
+
 #ifndef INT64_MAX
 #define INT64_MAX 0x7fffffffffffffffll
 #endif
@@ -48,17 +52,40 @@ typedef int64_t intmax_t;
 #define UINT64_MIN 0x0000000000000000ll
 #endif
 
-#ifndef SIZE_MAX
-#define SIZE_MAX (~(size_t)0)
+#ifndef INT32_MAX
+#define INT32_MAX ((int32_t)0x7fffffff)
 #endif
 
+#ifndef INT32_MIN
+#define INT32_MIN ((int32_t)0x80000000)
+#endif
 
 #ifndef UINT32_MAX
 #define UINT32_MAX 0xffffffff
 #endif
 
+#ifndef INT16_MAX
+#define INT16_MAX ((int16_t)0x7fff)
+#endif
+
+#ifndef INT16_MIN
+#define INT16_MIN ((int16_t)0x8000)
+#endif
+
 #ifndef UINT16_MAX
 #define UINT16_MAX 0xffff
+#endif
+
+#ifndef INT8_MAX
+#define INT8_MAX ((int8_t)0x7f)
+#endif
+
+#ifndef INT8_MIN
+#define INT8_MIN ((int8_t)0x80)
+#endif
+
+#ifndef UINT8_MAX
+#define UINT8_MAX 0xff
 #endif
 
 #endif
