3fba2ef41fa76d83e6d4d23792f205e0fe67f43a
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2019-11-22 13:49:22 +0100

    sys: added mount

--- sys/mount.c
+++ sys/mount.c
@@ -17,6 +17,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/file.h>
 #include <sys/msg.h>
@@ -24,49 +25,15 @@
 
 int mount(const char *source, const char *target, const char *fstype, long mode, char *data)
 {
-/*	struct stat buf;
-	oid_t toid, soid, doid, *doidp;
-	msg_t msg = {0};
-	int err;
+	int fd, err;
 
-	mount_msg_t *mnt = (mount_msg_t *)msg.i.raw;
+	fd = fsMount(fstype, AT_FDCWD, source, 0);
 
-	if (lookup(target, NULL, &toid) < EOK)
-		return -ENOENT;
+	if (fd < 0) {
+		return SET_ERRNO(fd);
+	}
 
-	if (lookup(source, NULL, &soid) < EOK)
-		return -ENOENT;
-
-	if ((err = stat(target, &buf)))
-		return err;
-
-	if (!S_ISDIR(buf.st_mode))
-		return -ENOTDIR;
-
-	msg.type = mtMount;
-	mnt->id = soid.id;
-	mnt->mode = mode;
-	strncpy(mnt->fstype, fstype, sizeof(mnt->fstype));
-
-	msg.i.size = data != NULL ? strlen(data) : 0;
-	msg.i.data = data;
-
-	if ((err = msgSend(soid.port, &msg)) < 0)
-		return err;
-
-	doidp = (oid_t *)msg.o.raw;
-	doid = *doidp;
-
-	memset(&msg, 0, sizeof(msg));
-
-	msg.type = mtSetAttr;
-	msg.i.attr.oid = toid;
-	msg.i.attr.type = atDev;
-	msg.i.data = &doid;
-	msg.i.size = sizeof(oid_t);
-
-	if ((err = msgSend(toid.port, &msg)) < 0)
-		return err;
-*/
-	return EOK;
+	err = fsBind(AT_FDCWD, target, fd, NULL);
+	close(fd);
+	return SET_ERRNO(err);
 }
