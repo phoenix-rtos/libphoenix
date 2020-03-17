0c86ed72866ef1f40c4eaa5e10a01f67073621c9
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2020-03-18 00:01:27 +0100

    unistd: added getopt_long implementation

--- unistd/getopt.c
+++ unistd/getopt.c
@@ -5,8 +5,8 @@
  *
  * unistd getopt
  *
- * Copyright 2018 Phoenix Systems
- * Author: Jan Sikorski, Krystian Wasik
+ * Copyright 2018, 2020 Phoenix Systems
+ * Author: Jan Sikorski, Krystian Wasik, Lukasz Kosinski
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -37,7 +37,7 @@ int getopt(int argc, char * const argv[], const char *optstring)
 	if (argc == 0 || argv == NULL || optind >= argc)
 		return -1;
 
-	if (argv[optind] == NULL || *argv[optind] != '-' || !strcmp(argv[optind], "-"))
+	if (argv[optind] == NULL || argv[optind][0] != '-' || argv[optind][1] == '\0')
 		return -1;
 
 	if (argv[optind][1] == '-') {
@@ -66,7 +66,7 @@ int getopt(int argc, char * const argv[], const char *optstring)
 
 		} else if (optind + 1 < argc) { /* Argument in the next argv */
 			optarg = argv[optind + 1];
-			optind ++;
+			optind++;
 
 		} else { /* Argument not provided */
 			optopt = opt;
@@ -92,7 +92,93 @@ int getopt(int argc, char * const argv[], const char *optstring)
 }
 
 
+static void rotate_right(char **dest, char **src)
+{
+	char *tmp = *src;
+
+	memmove(dest + 1, dest, (src - dest) * sizeof(char *));
+	*dest = tmp;
+}
+
+
 int getopt_long(int argc, char * const argv[], const char *optstring, const struct option *longopts, int *longindex)
 {
-	return -1;
+	char *opt, *name;
+	int skipped, resumed;
+	int i, colon, ret;
+
+	if (argc == 0 || argv == NULL)
+		return -1;
+
+	skipped = optind;
+	if (optstring[0] != '+' && optstring[0] != '-') {
+		for (i = optind; ; i++) {
+			if (i >= argc || argv[i] == NULL)
+				return -1;
+			if (argv[i][0] == '-' && argv[i][1])
+				break;
+		}
+		optind = i;
+	}
+	resumed = optind;
+
+	optarg = NULL;
+	if (longopts && argv[optind] != NULL && argv[optind][0] == '-' && argv[optind][1] == '-' && argv[optind][2]) {
+		colon = optstring[optstring[0] == '+' || optstring[0] == '-'] == ':';
+
+		for (i = 0; (name = (char *)longopts[i].name); i++) {
+			opt = argv[optind] + 2;
+			for (; *name && *name == *opt; name++, opt++);
+			if (*opt && *opt != '=')
+				continue;
+			if (!*name)
+				break;
+		}
+
+		if (name != NULL) {
+			optind++;
+			optopt = longopts[i].val;
+
+			if (*opt == '=') {
+				if (!longopts[i].has_arg) {
+					if (!colon && opterr)
+						fprintf(stderr, "%s: option does not take an argument -- %s\n", argv[0], longopts[i].name);
+					return '?';
+				}
+				optarg = opt + 1;
+			}
+			else if (longopts[i].has_arg == required_argument){
+				if (!(optarg = argv[optind])) {
+					if (colon)
+						return ':';
+					if (opterr)
+						fprintf(stderr, "%s: option requires an argument -- %s\n", argv[0], longopts[i].name);
+					return '?';
+				}
+				optind++;
+			}
+
+			if (longindex)
+				*longindex = i;
+			if (longopts[i].flag) {
+				*longopts[i].flag = longopts[i].val;
+				return 0;
+			}
+			return longopts[i].val;
+		}
+
+		if (!colon && opterr)
+			fprintf(stderr, "%s: illegal option -- %s\n", argv[0], argv[optind] + 2);
+		optind++;
+		return '?';
+	}
+	ret = getopt(argc, argv, optstring);
+
+	if (resumed > skipped) {
+		for (i = 0; i < optind - resumed; i++)
+			rotate_right((char **)argv + skipped, (char **)argv + optind - 1);
+		optind -= resumed - skipped;
+	}
+
+	return ret;
 }
