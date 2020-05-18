edc1fed817ebd38d0753a4899cf0ce58bce75402
Pawel Pisarczyk <pawel.pisarczyk@phoesys.com>
2020-05-18 20:30:48 +0200

    Adding int_fast types (C99) for RISCV64 - required by riscv64-pk

--- include/arch/riscv64/stdint.h
+++ include/arch/riscv64/stdint.h
@@ -21,10 +21,24 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
 
+typedef uint8_t uint_fast8_t;
+typedef uint16_t uint_fast16_t;
+typedef uint32_t uint_fast32_t;
+typedef uint64_t uint_fast64_t;
+
+typedef uint8_t uint_least8_t;
+
 typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
 typedef long int64_t;
+
+typedef int8_t int_fast8_t;
+typedef int16_t int_fast16_t;
+typedef int32_t int_fast32_t;
+typedef int64_t int_fast64_t;
+
+#define UINT64_C(c) c ## ULL
 
 /* Define other fixed types along with defines/macros */
 
