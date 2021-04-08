26f220d3b6531d5e05d54dc663c2c45a958cefa5
tomasz korniluk <tomasz.korniluk@phoenix-rtos.com>
2021-04-08 16:02:06 +0200

    added host-pc target
    JIRA: ISC-64

---
+++ build-core-host-pc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Shell script for building Phoenix-RTOS firmware
+#
+# Builder for Phoenix-RTOS core components
+#
+# Copyright 2021 Phoenix Systems
+# Author: Tomasz Korniluk
+#
+
+# fail immediately if any of the commands fails
+set -e
+
+b_log "Building libphoenix"
+(cd libphoenix && make -f Makefile.host $MAKEFLAGS $CLEAN all)
+
+b_log "Building phoenix-rtos-filesystems"
+(cd phoenix-rtos-filesystems && make $MAKEFLAGS $CLEAN all)
+
+b_log "Building phoenix-rtos-devices"
+(cd phoenix-rtos-devices && make $MAKEFLAGS $CLEAN all)
