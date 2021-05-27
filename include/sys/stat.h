1d2172ad7a1832b52fd86b685dd6a1dbcc0643df
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2021-05-27 13:47:21 +0200

    stat: make struct stat POSIX-2008 compliant
    JIRA: RTOS-29

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -80,12 +80,16 @@ struct stat {
 	gid_t     st_gid;
 	dev_t     st_rdev;
 	off_t     st_size;
-	time_t    st_atime;
-	time_t    st_mtime;
-	time_t    st_ctime;
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
 	blksize_t st_blksize;
 	blkcnt_t  st_blocks;
 };
+
+#define st_atime st_atim.tv_sec
+#define st_ctime st_ctim.tv_sec
+#define st_mtime st_mtim.tv_sec
 
 
 extern int lstat(const char *path, struct stat *buf);
