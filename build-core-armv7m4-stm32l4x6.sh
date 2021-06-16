d6917ffb6d3572ec181848730288f7110f7cfae7
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2021-06-16 12:52:11 +0200

    armv7m4-stm32l4x6: add building corelibs
    JIRA: PD-119

--- build-core-armv7m4-stm32l4x6.sh
+++ build-core-armv7m4-stm32l4x6.sh
@@ -18,6 +18,9 @@ make -C "phoenix-rtos-kernel" $KERNEL_MAKECMDGOALS all
 b_log "Building libphoenix"
 make -C "libphoenix" all install
 
+b_log "Building phoenix-rtos-corelibs"
+make -C "phoenix-rtos-corelibs" all install
+
 b_log "Building phoenix-rtos-devices"
 make -C "phoenix-rtos-devices" all
 
