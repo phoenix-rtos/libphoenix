17335077db0f7e49c5337cfc03bfb8f2fe3cb8f3
Andrzej Glowinski <andrzej.glowinski@phoenix-rtos.com>
2021-07-05 10:35:58 +0200

    scripts: Fix stm32l4 program script
    program script uses absolute predefined path to opemocd scripts before.
    Now it will be relative to openocd installation.
    
    JIRA: ISC-118

--- scripts/program-stm32l4x6.sh
+++ scripts/program-stm32l4x6.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-OPENOCDPATH="/usr/local/share/openocd"
-
-openocd -f $OPENOCDPATH/scripts/interface/stlink.cfg -f $OPENOCDPATH/scripts/target/stm32l4x.cfg -c "reset_config srst_only srst_nogate connect_assert_srst"  -c"program $1 0x08000000 verify reset exit"
+openocd -f interface/stlink.cfg -f target/stm32l4x.cfg -c "reset_config srst_only srst_nogate connect_assert_srst"  -c"program $1 0x08000000 verify reset exit"
 
 exit 0
