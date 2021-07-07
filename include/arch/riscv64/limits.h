6f5aca8aff0a2059cc8cc6cb2d6bbb398d45c828
damian.loewnau <damian.loewnau@gerrit.phoesys.com>
2021-07-07 16:42:04 +0200

    include: apply clang-format for limits.h files
    JIRA: RTOS-59

--- include/arch/riscv64/limits.h
+++ include/arch/riscv64/limits.h
@@ -28,33 +28,33 @@
 
 #define MB_LEN_MAX 4
 
-#define SHRT_MIN -32768
-#define SHRT_MAX 32767
+#define SHRT_MIN  -32768
+#define SHRT_MAX  32767
 #define USHRT_MAX 65535
 
-#define INT_MIN -2147483648
-#define INT_MAX 0x7fffffff
+#define INT_MIN  -2147483648
+#define INT_MAX  0x7fffffff
 #define UINT_MAX 0xffffffff
 
-#define LONG_MIN 0x8000000000000000L
-#define LONG_MAX 0x7fffffffffffffffL
+#define LONG_MIN  0x8000000000000000L
+#define LONG_MAX  0x7fffffffffffffffL
 #define ULONG_MAX 0xffffffffffffffffL
 
-#define LONG_LONG_MIN LONG_MIN
-#define LONG_LONG_MAX LONG_MAX
+#define LONG_LONG_MIN  LONG_MIN
+#define LONG_LONG_MAX  LONG_MAX
 #define ULONG_LONG_MAX ULONG_MAX
-#define LLONG_MIN LONG_LONG_MIN
-#define LLONG_MAX LONG_LONG_MAX
-#define ULLONG_MAX ULONG_LONG_MAX
+#define LLONG_MIN      LONG_LONG_MIN
+#define LLONG_MAX      LONG_LONG_MAX
+#define ULLONG_MAX     ULONG_LONG_MAX
 
 #define SSIZE_MAX INT_MAX
 
-#define PTHREAD_STACK_MIN 256
-#define PATH_MAX 1024
-#define ARG_MAX 128
+#define PTHREAD_STACK_MIN  256
+#define PATH_MAX           1024
+#define ARG_MAX            128
 #define _POSIX2_RE_DUP_MAX 255
 
 #define PAGE_SIZE _PAGE_SIZE
-#define PAGESIZE _PAGE_SIZE
+#define PAGESIZE  _PAGE_SIZE
 
 #endif
