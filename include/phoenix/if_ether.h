44f537b30e7c2dcc0712ae71a26f506a0a4f3bfa
ziemowit.leszczynski <ziemowit.leszczynski@phoenix-rtos.com>
2021-07-23 10:14:33 +0200

    socket: add missing packet sockets definitions
    Also:
     - format code
     - switch from ETHERTYPE_ALL to ETH_P_ALL
    
    JIRA: DTR-94

---
+++ include/phoenix/if_ether.h
@@ -0,0 +1,21 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * phoenix/if_ether.h
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Ziemowit Leszczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_PHOENIX_IF_ETHER_H_
+#define _LIBPHOENIX_PHOENIX_IF_ETHER_H_
+
+#define ETH_P_ALL 0x0003 /* Every packet (be careful!!!) */
+
+#endif
