3589ec72d499f417db422112d80077ea3409279d
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-10-20 11:37:42 +0200

    zynq7000: add to building process - devices, filesystems and corelibs
    JIRA: PD-6

--- build-core-armv7a9-zynq7000.sh
+++ build-core-armv7a9-zynq7000.sh
@@ -18,8 +18,17 @@ make -C "phoenix-rtos-kernel" $KERNEL_MAKECMDGOALS all
 b_log "Building libphoenix"
 make -C "libphoenix" all install
 
+b_log "Building phoenix-rtos-filesystems"
+make -C "phoenix-rtos-filesystems" all install
+
+b_log "Building phoenix-rtos-devices"
+make -C "phoenix-rtos-devices" all install
+
 b_log "Building phoenix-rtos-corelibs"
 make -C "phoenix-rtos-corelibs" all install
+
+b_log "Building coreutils"
+make -C "phoenix-rtos-utils" all install
 
 b_log "Building plo"
 make -C "plo" all
