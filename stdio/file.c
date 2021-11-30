phoenix-rtos/libphoenix

commit 86482f2b828a755be215447a8c0a0442a7a4f42b

lookup: correct usage of lookup when target should be fle
JIRA: BES-187

stdio/file.c
@@ -1076,7 +1076,7 @@ FILE *tmpfile(void)
 {
 	oid_t oid;
 
-	while (lookup("/dev/posix/tmpfile", NULL, &oid) < 0) {
+	while (lookup("/dev/posix/tmpfile", &oid, NULL) < 0) {
 		if (errno != EINTR)
 			return NULL;
 	}

unistd/dir.c
@@ -375,7 +375,7 @@ DIR *opendir(const char *dirname)
 		return NULL; /* errno set by resolve_path */
 	}
 
-	if (!dirname[0] || (safe_lookup(canonical_name, NULL, &s->oid) < 0)) {
+	if (!dirname[0] || (safe_lookup(canonical_name, &s->oid, NULL) < 0)) {
 		free(canonical_name);
 		free(s);
 		errno = ENOENT;
@@ -453,7 +453,7 @@ static ssize_t _readlink_abs(const char *path, char *buf, size_t bufsiz)
 
 	assert(path && path[0] == '/');
 
-	if ((ret = safe_lookup(path, NULL, &oid)) < 0)
+	if ((ret = safe_lookup(path, &oid, NULL)) < 0)
 		return SET_ERRNO(ret);
 
 	msg.type = mtGetAttr;
@@ -517,7 +517,7 @@ int rmdir(const char *path)
 
 	splitname(canonical_name, &name, &dirname);
 
-	if (safe_lookup(dirname, NULL, &dir)) {
+	if (safe_lookup(dirname, &dir, NULL)) {
 		free(canonical_name);
 		return SET_ERRNO(-ENOENT);
 	}

unistd/file.c
@@ -169,7 +169,7 @@ int symlink(const char *target, const char *linkpath)
 
 	splitname(canonical_linkpath, &name, &dir_name);
 
-	if ((ret = lookup(dir_name, NULL, &dir)) < 0) {
+	if ((ret = lookup(dir_name, &dir, NULL)) < 0) {
 		free(canonical_linkpath);
 		return SET_ERRNO(ret);
 	}

stdlib/mktemp.c
@@ -53,7 +53,7 @@ static int mktemp(char *template, int suffixlen)
 
 	close(fd);
 
-	if (lookup(template, NULL, &oid) == EOK)
+	if (lookup(template, &oid, NULL) == EOK)
 		return -EEXIST;
 
 	return EOK;

commit 5f62c3aba206f80087cf839e65ec3ea183fe1fc1

lookup: fix for errors introduced in 86482f2b828a755be215447a8c0a0442a7a4f42b
JIRA: DTR-180

stdio/file.c
@@ -1074,12 +1074,16 @@ void funlockfile(FILE *file)
 
 FILE *tmpfile(void)
 {
-	oid_t oid;
+	oid_t oid, dev;
 
-	while (lookup("/dev/posix/tmpfile", &oid, NULL) < 0) {
+	while (lookup("/dev/posix/tmpfile", &oid, &dev) < 0) {
 		if (errno != EINTR)
 			return NULL;
 	}
+
+	/* Make sure it's a device (created by posixsrv) */
+	if (oid.port == dev.port)
+		return NULL;
 
 	return fopen("/dev/posix/tmpfile", "w+");
 }

unistd/dir.c
@@ -375,7 +375,7 @@ DIR *opendir(const char *dirname)
 		return NULL; /* errno set by resolve_path */
 	}
 
-	if (!dirname[0] || (safe_lookup(canonical_name, &s->oid, NULL) < 0)) {
+	if (!dirname[0] || (safe_lookup(canonical_name, NULL, &s->oid) < 0)) {
 		free(canonical_name);
 		free(s);
 		errno = ENOENT;
@@ -521,6 +521,8 @@ int rmdir(const char *path)
 		free(canonical_name);
 		return SET_ERRNO(-ENOENT);
 	}
+
+	/* TODO: add proper mount point handling */
 
 	msg.type = mtUnlink;
 	memcpy(&msg.i.ln.dir, &dir, sizeof(dir));

unistd/file.c
@@ -169,7 +169,7 @@ int symlink(const char *target, const char *linkpath)
 
 	splitname(canonical_linkpath, &name, &dir_name);
 
-	if ((ret = lookup(dir_name, &dir, NULL)) < 0) {
+	if ((ret = lookup(dir_name, NULL, &dir)) < 0) {
 		free(canonical_linkpath);
 		return SET_ERRNO(ret);
 	}