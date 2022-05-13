de17e18255931e937dbdab1a96382fe9d0fa513a
Damian <damian.loewnau@phoenix-rtos.com>
2022-05-13 12:47:06 +0200

    include: inttypes: add SCN defines
    JIRA: PD-272

--- include/inttypes.h
+++ include/inttypes.h
@@ -53,6 +53,78 @@
 #define PRIi64 _PRI_64 "i"
 #define PRIo64 _PRI_64 "o"
 
+#define SCNd8 _SCN_8 "d"
+#define SCNu8 _SCN_8 "u"
+#define SCNx8 _SCN_8 "x"
+#define SCNi8 _SCN_8 "i"
+#define SCNo8 _SCN_8 "o"
+
+#define SCNdLEAST8 _SCN_LEAST8 "d"
+#define SCNuLEAST8 _SCN_LEAST8 "u"
+#define SCNxLEAST8 _SCN_LEAST8 "x"
+#define SCNiLEAST8 _SCN_LEAST8 "i"
+#define SCNoLEAST8 _SCN_LEAST8 "o"
+
+#define SCNdFAST8 _SCN_FAST8 "d"
+#define SCNuFAST8 _SCN_FAST8 "u"
+#define SCNxFAST8 _SCN_FAST8 "x"
+#define SCNiFAST8 _SCN_FAST8 "i"
+#define SCNoFAST8 _SCN_FAST8 "o"
+
+#define SCNd16 _SCN_16 "d"
+#define SCNu16 _SCN_16 "u"
+#define SCNx16 _SCN_16 "x"
+#define SCNi16 _SCN_16 "i"
+#define SCNo16 _SCN_16 "o"
+
+#define SCNdLEAST16 _SCN_LEAST16 "d"
+#define SCNuLEAST16 _SCN_LEAST16 "u"
+#define SCNxLEAST16 _SCN_LEAST16 "x"
+#define SCNiLEAST16 _SCN_LEAST16 "i"
+#define SCNoLEAST16 _SCN_LEAST16 "o"
+
+#define SCNdFAST16 _SCN_FAST16 "d"
+#define SCNuFAST16 _SCN_FAST16 "u"
+#define SCNxFAST16 _SCN_FAST16 "x"
+#define SCNiFAST16 _SCN_FAST16 "i"
+#define SCNoFAST16 _SCN_FAST16 "o"
+
+#define SCNd32 _SCN_32 "d"
+#define SCNu32 _SCN_32 "u"
+#define SCNx32 _SCN_32 "x"
+#define SCNi32 _SCN_32 "i"
+#define SCNo32 _SCN_32 "o"
+
+#define SCNdLEAST32 _SCN_LEAST32 "d"
+#define SCNuLEAST32 _SCN_LEAST32 "u"
+#define SCNxLEAST32 _SCN_LEAST32 "x"
+#define SCNiLEAST32 _SCN_LEAST32 "i"
+#define SCNoLEAST32 _SCN_LEAST32 "o"
+
+#define SCNdFAST32 _SCN_FAST32 "d"
+#define SCNuFAST32 _SCN_FAST32 "u"
+#define SCNxFAST32 _SCN_FAST32 "x"
+#define SCNiFAST32 _SCN_FAST32 "i"
+#define SCNoFAST32 _SCN_FAST32 "o"
+
+#define SCNd64 _SCN_64 "d"
+#define SCNu64 _SCN_64 "u"
+#define SCNx64 _SCN_64 "x"
+#define SCNi64 _SCN_64 "i"
+#define SCNo64 _SCN_64 "o"
+
+#define SCNdLEAST64 _SCN_LEAST64 "d"
+#define SCNuLEAST64 _SCN_LEAST64 "u"
+#define SCNxLEAST64 _SCN_LEAST64 "x"
+#define SCNiLEAST64 _SCN_LEAST64 "i"
+#define SCNoLEAST64 _SCN_LEAST64 "o"
+
+#define SCNdFAST64 _SCN_FAST64 "d"
+#define SCNuFAST64 _SCN_FAST64 "u"
+#define SCNxFAST64 _SCN_FAST64 "x"
+#define SCNiFAST64 _SCN_FAST64 "i"
+#define SCNoFAST64 _SCN_FAST64 "o"
+
 #endif
 
 extern intmax_t strtoimax(const char *nptr, char **endptr, int base);
