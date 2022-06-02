49417dd1a1dc3ccca96be23b79327fbc50dde391
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2022-06-02 10:53:41 +0200

    include: add TCP_KEEPIDLE and TCP_KEEPINTVL to tcp.h
    JIRA: PD-251
    
    It's required for azure iot sdk c incoming port

--- include/netinet/tcp.h
+++ include/netinet/tcp.h
@@ -5,8 +5,8 @@
  *
  * tcp.h
  *
- * Copyright 2018 Phoenix Systems
- * Author: Kamil Amanowicz
+ * Copyright 2018, 2022 Phoenix Systems
+ * Author: Kamil Amanowicz, Damian Loewnau
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -16,6 +16,8 @@
 #ifndef _LIBPHOENIX_NETINET_TCP_H_
 #define _LIBPHOENIX_NETINET_TCP_H_
 
-#define TCP_NODELAY 1
+#define TCP_NODELAY   0x1
+#define TCP_KEEPIDLE  0x3
+#define TCP_KEEPINTVL 0x4
 
 #endif
