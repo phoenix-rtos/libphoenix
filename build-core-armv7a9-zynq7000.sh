63ea0c20a3947dfe1a0acb8c7985f695f8c4e836
Maciej Purski <maciej.purski@phoenix-rtos.com>
2022-06-21 13:36:08 +0200

    armv7a9-zynq7000: Build posixsrv
    JIRA: PP-45

--- build-core-armv7a9-zynq7000.sh
+++ build-core-armv7a9-zynq7000.sh
@@ -33,6 +33,9 @@ make -C "phoenix-rtos-devices" all install
 b_log "Building coreutils"
 make -C "phoenix-rtos-utils" all install
 
+b_log "Building posixsrv"
+make -C "phoenix-rtos-posixsrv" all install
+
 # FIXME: compile host tools using host-generic-pc target?
 b_log "Building hostutils"
 make -C "phoenix-rtos-hostutils" -f Makefile.old $CLEAN all
