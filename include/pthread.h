493aa6d557c236f5247c03fc7abe55cb826023d9
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2022-07-15 12:13:02 +0200

    pthread: format existing code
    JIRA: RTOS-179

--- include/pthread.h
+++ include/pthread.h
@@ -27,28 +27,28 @@
 #define PTHREAD_CREATE_DETACHED 1
 
 #define PTHREAD_EXPLICIT_SCHED 0
-#define PTHREAD_INHERIT_SCHED 1
+#define PTHREAD_INHERIT_SCHED  1
 
-#define PTHREAD_PRIO_NONE 0
+#define PTHREAD_PRIO_NONE    0
 #define PTHREAD_PRIO_INHERIT 1
 #define PTHREAD_PRIO_PROTECT 2
 
-#define PTHREAD_PROCESS_SHARED 0
+#define PTHREAD_PROCESS_SHARED  0
 #define PTHREAD_PROCESS_PRIVATE 1
 
 #define PTHREAD_SCOPE_PROCESS 0
-#define PTHREAD_SCOPE_SYSTEM 1
+#define PTHREAD_SCOPE_SYSTEM  1
 
 #define PTHREAD_MUTEX_INITIALIZER 0
 
-#define PTHREAD_MUTEX_DEFAULT 0
+#define PTHREAD_MUTEX_DEFAULT    0
 #define PTHREAD_MUTEX_ERRORCHECK 1
-#define PTHREAD_MUTEX_NORMAL 2
-#define PTHREAD_MUTEX_RECURSIVE 3
+#define PTHREAD_MUTEX_NORMAL     2
+#define PTHREAD_MUTEX_RECURSIVE  3
 
 
-extern int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-	void *(*start_routine)(void *), void *arg);
+
+extern int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
 
 
 extern int pthread_join(pthread_t thread, void **value_ptr);
@@ -75,70 +75,58 @@ extern int pthread_attr_destroy(pthread_attr_t *attr);
 extern int pthread_attr_setstackaddr(pthread_attr_t *attr, void *stackaddr);
 
 
-extern int pthread_attr_getstackaddr(const pthread_attr_t *attr,
-	void **stackaddr);
+extern int pthread_attr_getstackaddr(const pthread_attr_t *attr, void **stackaddr);
 
 
 extern int pthread_attr_setstacksize(pthread_attr_t *attr, size_t stacksize);
 
 
-extern int pthread_attr_getstacksize(const pthread_attr_t *attr,
-	size_t *stacksize);
+extern int pthread_attr_getstacksize(const pthread_attr_t *attr, size_t *stacksize);
 
 
-extern int pthread_attr_setschedparam(pthread_attr_t *attr,
-	const struct sched_param *param);
+extern int pthread_attr_setschedparam(pthread_attr_t *attr, const struct sched_param *param);
 
 
-extern int pthread_attr_getschedparam(const pthread_attr_t *attr,
-	struct sched_param *param);
+extern int pthread_attr_getschedparam(const pthread_attr_t *attr, struct sched_param *param);
 
 
 extern int pthread_attr_setschedpolicy(pthread_attr_t *attr, int policy);
 
 
-extern int pthread_attr_getschedpolicy(const pthread_attr_t *attr,
-	int *policy);
+extern int pthread_attr_getschedpolicy(const pthread_attr_t *attr, int *policy);
 
 
-extern int pthread_attr_setscope(pthread_attr_t *attr, int contentionscope); //TODO
+extern int pthread_attr_setscope(pthread_attr_t *attr, int contentionscope); /* TODO */
 
 
-extern int pthread_attr_getscope(const pthread_attr_t *attr,
-	int *contentionscope); //TODO
+extern int pthread_attr_getscope(const pthread_attr_t *attr, int *contentionscope); /* TODO */
 
 
 extern int pthread_attr_setdetachstate(pthread_attr_t *attr, int detachstate);
 
 
-extern int pthread_attr_getdetachstate(const pthread_attr_t *attr,
-	int *detachstate);
+extern int pthread_attr_getdetachstate(const pthread_attr_t *attr, int *detachstate);
 
 
-extern int pthread_attr_getinheritsched(const pthread_attr_t *restrict attr,
-	int *restrict inheritsched); //TODO
+extern int pthread_attr_getinheritsched(const pthread_attr_t *restrict attr, int *restrict inheritsched); /* TODO */
 
 
-extern int pthread_attr_setinheritsched(pthread_attr_t *attr,
-	int inheritsched); //TODO
+extern int pthread_attr_setinheritsched(pthread_attr_t *attr, int inheritsched); /* TODO */
 
 
 extern int pthread_setschedprio(pthread_t thread, int prio);
 
 
-extern int pthread_getschedparam(pthread_t thread, int *policy,
-	struct sched_param *restrict param);
+extern int pthread_getschedparam(pthread_t thread, int *policy, struct sched_param *restrict param);
 
 
-extern int pthread_setschedparam(pthread_t thread, int policy,
-	const struct sched_param *param);
+extern int pthread_setschedparam(pthread_t thread, int policy, const struct sched_param *param);
 
 
 extern int pthread_mutex_destroy(pthread_mutex_t *);
 
 
-extern int pthread_mutex_init(pthread_mutex_t *restrict,
-	const pthread_mutexattr_t *restrict);
+extern int pthread_mutex_init(pthread_mutex_t *restrict, const pthread_mutexattr_t *restrict);
 
 
 extern int pthread_mutex_lock(pthread_mutex_t *);
