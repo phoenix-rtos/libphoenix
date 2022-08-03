9caee5a8ad2be87c7e1e99d3c4e8732a1064f215
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2022-08-03 11:36:28 +0200

    Add perror implementation
    DONE: RTOS-229

--- stdio/perror.c
+++ stdio/perror.c
@@ -5,14 +5,19 @@
  *
  * perror.c
  *
- * Copyright 2018 Phoenix Systems
- * Author: Kamil Amanowicz
+ * Copyright 2022 Phoenix Systems
+ * Author: Aleksander Kaminski
  *
  * This file is part of Phoenix-RTOS.
  *
  * %LICENSE%
  */
 
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
 void perror(const char *str)
 {
+	fprintf(stderr, "%s: %s\n", str, strerror(errno));
 }
