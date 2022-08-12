9f363a51852debe549fce625a4ecc07fe64c9e6d
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2022-08-12 15:59:29 +0200

    errno: add support for errno in TLS
    JIRA: RTOS-209

--- errno/errno.c
+++ errno/errno.c
@@ -15,7 +15,15 @@
 
 #include <sys/rb.h>
 #include <sys/threads.h>
+#include <errno.h>
+#include <arch.h>
 
+
+#ifdef __LIBPHOENIX_ARCH_TLS_SUPPORTED
+
+static __thread int __errno_tls;
+
+#else
 
 static int errno_global;
 
@@ -32,10 +40,12 @@ static int errno_cmp(rbnode_t *n1, rbnode_t *n2)
 
 	return (e1->tid < e2->tid) - (e1->tid > e2->tid);
 }
+#endif
 
 
 int *__errno_location(void)
 {
+#ifndef __LIBPHOENIX_ARCH_TLS_SUPPORTED
 	struct __errno_t *e, r;
 	r.tid = gettid();
 
@@ -47,30 +57,39 @@ int *__errno_location(void)
 		return &e->no;
 
 	return &errno_global;
+#else
+	return &__errno_tls;
+#endif
 }
 
 
 void _errno_new(struct __errno_t *e)
 {
+#ifndef __LIBPHOENIX_ARCH_TLS_SUPPORTED
 	e->no = 0;
 	e->tid = gettid();
 
 	mutexLock(errno_common.lock);
 	lib_rbInsert(&errno_common.tree, &e->linkage);
 	mutexUnlock(errno_common.lock);
+#endif
 }
 
 
 void _errno_remove(struct __errno_t *e)
 {
+#ifndef __LIBPHOENIX_ARCH_TLS_SUPPORTED
 	mutexLock(errno_common.lock);
 	lib_rbRemove(&errno_common.tree, &e->linkage);
 	mutexUnlock(errno_common.lock);
+#endif
 }
 
 
 void _errno_init(void)
 {
+#ifndef __LIBPHOENIX_ARCH_TLS_SUPPORTED
 	mutexCreate(&errno_common.lock);
 	lib_rbInit(&errno_common.tree, errno_cmp, NULL);
+#endif
 }
