6a6b31ab57d94f526dcc104222e548ceafb2dd39
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2023-06-19 13:47:38 +0200

    math: Add optional per arch sqrt implementation
    Add implementation for armv7
    
    DONE: RTOS-495

--- include/arch/arm-imx/arch.h
+++ include/arch/arm-imx/arch.h
@@ -33,6 +33,17 @@
 #define __STRNCPY
 #define __MEMMOVE
 
+#if (__ARM_PCS_VFP || (__VFP_FP__ && !__SOFTFP__)) && (__ARM_FP & 8)
+#define __IEEE754_SQRT
+
+static inline double __ieee754_sqrt(double x)
+{
+	__asm__ volatile ("vsqrt.f64 %P0, %P1" : "=w"(x) : "w"(x));
+
+	return x;
+}
+#endif
+
 #define _PAGE_SIZE 0x1000
 #define SIZE_PAGE _Pragma("GCC warning \"'SIZE_PAGE' is deprecated. Use _PAGE_SIZE from arch.h or PAGE_SIZE from limits.h (POSIX only)\"") _PAGE_SIZE
 
