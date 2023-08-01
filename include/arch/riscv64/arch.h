822d7df84c5b0de1fceb6bb2183912dc9180ec07
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-08-01 10:32:07 +0200

    riscv64: use FPU to calculate sqrt
    JIRA: RTOS-498

--- include/arch/riscv64/arch.h
+++ include/arch/riscv64/arch.h
@@ -26,6 +26,31 @@
 #define __MEMCPY
 #define __MEMSET
 
+#if (!__SOFTFP__)
+#define __IEEE754_SQRT
+
+static inline double __ieee754_sqrt(double x)
+{
+	/* clang-format off */
+	__asm__ volatile ("fsqrt.d %0, %1" : "=f"(x) : "f"(x));
+	/* clang-format on */
+
+	return x;
+}
+
+
+#define __IEEE754_SQRTF
+static inline float __ieee754_sqrtf(float x)
+{
+	/* clang-format off */
+	__asm__ volatile ("fsqrt.s %0, %1" : "=f"(x) : "f"(x));
+	/* clang-format on */
+
+	return x;
+}
+
+#endif
+
 #define _PAGE_SIZE 0x1000
 #define SIZE_PAGE _Pragma("GCC warning \"'SIZE_PAGE' is deprecated. Use _PAGE_SIZE from arch.h or PAGE_SIZE from limits.h (POSIX only)\"") _PAGE_SIZE
 
