f691457f7a1cfe195a84465303cfae516a353064
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2023-09-13 16:32:26 +0200

    dirent: use common d_type defines
    - already used internally in jffs2
     - not part of POSIX but needed for interoperability with GNU tools
     - (especially autoconfig/automake in libstdc++ gcc-9.5.0)
    
    JIRA: CI-334

--- include/dirent.h
+++ include/dirent.h
@@ -28,12 +28,25 @@ extern "C" {
 
 
 /* dirent types */
+/* FIXME: remove when dirent support would be straightened out in filesystems */
 enum {	dtDir = 0,
 		dtFile,
 		dtDev,
 		dtSymlink,
 		dtUnknown
 };
+
+/* NOTE: these values can't be changed as they are kept on FLASH (eg. in jffs2) */
+#define DT_UNKNOWN 0
+#define DT_FIFO    1
+#define DT_CHR     2
+#define DT_DIR     4
+#define DT_BLK     6
+#define DT_REG     8
+#define DT_LNK     10
+#define DT_SOCK    12
+#define DT_WHT     14
+
 
 struct dirent {
 	ino_t  d_ino;
