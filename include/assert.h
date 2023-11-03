417f7c3f37eb8fd571db1350df433b5d719f4818
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2023-11-03 13:15:29 +0100

    assert: use abort() instead of exit()
    - atexit functions should not be called
    - stream synchronization is optional (might lead to deadlocks)
    - the exit code should indicate the program was killed with SIGABRT
    
    JIRA: RTOS-668

--- include/assert.h
+++ include/assert.h
@@ -28,9 +28,7 @@ extern "C" {
 
 #ifndef NDEBUG
 #define assert(__expr) \
-    ((__expr) \
-     ? (void)0 \
-     : ({ printf("Assertion '%s' failed in file %s:%d, function %s.\n", #__expr, __FILE__, __LINE__, __func__); exit(1);}))
+	((__expr) ? (void)0 : ({ printf("Assertion '%s' failed in file %s:%d, function %s.\n", #__expr, __FILE__, __LINE__, __func__); abort(); }))
 #else
 
 
