5282fbbf369191d7a22aaed392cbb36877429de7
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2024-01-23 14:21:40 +0100

    string/strstr: handle empty special case
    JIRA: RTOS-743

--- string/string.c
+++ string/string.c
@@ -327,6 +327,10 @@ char *strstr(const char *s1, const char *s2)
 {
 	const char *p1, *p2;
 
+	if (*s2 == '\0') {
+		return (char *)s1;
+	}
+
 	for (; *s1; ++s1) {
 		for (p1 = s1, p2 = s2; *p2 && *p1 && *p1 == *p2; ++p1, ++p2)
 			;
