41d840e288e27057c286fbb9cc106f643f22c035
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-02-05 11:52:19 +0100

    !sys/interrupt: update function signature
    JIRA: RTOS-125

--- include/sys/interrupt.h
+++ include/sys/interrupt.h
@@ -5,8 +5,8 @@
  *
  * sys/interrupt
  *
- * Copyright 2017 Phoenix Systems
- * Author: Aleksander Kaminski
+ * Copyright 2017, 2024 Phoenix Systems
+ * Author: Aleksander Kaminski, Lukasz Leczkowski
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -17,12 +17,15 @@
 #define _LIBPHOENIX_SYS_INTERRUPT_H_
 
 
+#include <sys/types.h>
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
-extern int interrupt(unsigned int n, int (*f)(unsigned int, void *), void *arg, unsigned int cond, unsigned int *handle);
+extern int interrupt(unsigned int n, int (*f)(unsigned int, void *), void *arg, handle_t cond, handle_t *handle);
 
 
 #ifdef __cplusplus
