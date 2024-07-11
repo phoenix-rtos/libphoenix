789fb47ba058e23057a041ae7a220a5eebdfa170
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-07-11 11:53:46 +0200

    time: implement clock_settime
    JIRA: RTOS-867

--- include/time.h
+++ include/time.h
@@ -103,6 +103,9 @@ extern time_t timegm(struct tm *tm);
 extern int clock_gettime(clockid_t clk_id, struct timespec *tp);
 
 
+extern int clock_settime(clockid_t clock_id, const struct timespec *tp);
+
+
 extern time_t time(time_t *tp);
 
 
