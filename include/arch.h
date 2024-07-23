d8908ae4cc80739e6abbb7ec4a8564bcb192f697
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-07-23 17:19:21 +0200

    arch: add ARMv8-R support
    JIRA: RTOS-875

--- include/arch.h
+++ include/arch.h
@@ -6,6 +6,8 @@
 #include <arch/armv7a/arch.h>
 #elif defined(__ARM_ARCH_4T__) || defined(__ARM_ARCH_5TE__) /* not currently supported, map to 7M for libgcc to compile */
 #include <arch/armv7m/arch.h>
+#elif defined(__ARM_ARCH_8R__)
+#include <arch/armv8r/arch.h>
 #elif defined(__riscv) && (__riscv_xlen == 64)
 #include <arch/riscv64/arch.h>
 #elif defined(__sparc__)
---
+++ include/arch/armv8r/arch.h
@@ -0,0 +1,64 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * Architecture dependent part (arch/armv8r)
+ *
+ * Copyright 2017, 2024 Phoenix Systems
+ * Author: Pawel Pisarczyk, Lukasz Leczkowski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_ARCH_ARMV8R_ARCH_H_
+#define _LIBPHOENIX_ARCH_ARMV8R_ARCH_H_
+
+#define __ARCH_STDINT <arch/armv8r/stdint.h>
+#define __ARCH_LIMITS <arch/armv8r/limits.h>
+
+#define __MEMCPY
+#define __MEMCMP
+#define __MEMSET
+#define __STRLEN
+#define __STRNLEN
+#define __STRCMP
+#define __STRNCMP
+#define __STRCPY
+#define __STRNCPY
+#define __MEMMOVE
+
+#if __ARM_PCS_VFP || (__VFP_FP__ && !__SOFTFP__)
+#if __ARM_FP & 8
+#define __IEEE754_SQRT
+
+static inline double __ieee754_sqrt(double x)
+{
+	/* clang-format off */
+	__asm__ volatile ("vsqrt.f64 %P0, %P1" : "=w"(x) : "w"(x));
+	/* clang-format on */
+
+	return x;
+}
+#endif
+
+#define __IEEE754_SQRTF
+
+static inline float __ieee754_sqrtf(float x)
+{
+	/* clang-format off */
+	__asm__ volatile ("vsqrt.f32 %0, %1" : "=t"(x) : "t"(x));
+	/* clang-format on */
+
+	return x;
+}
+#endif
+
+#define _PAGE_SIZE 0x1000
+#define SIZE_PAGE  _Pragma("GCC warning \"'SIZE_PAGE' is deprecated. Use _PAGE_SIZE from arch.h or PAGE_SIZE from limits.h (POSIX only)\"") _PAGE_SIZE
+
+#define __LIBPHOENIX_ARCH_TLS_SUPPORTED
+
+#endif
