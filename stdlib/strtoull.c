40760a8523e7a0a7a02d960b3888fc37dff14c05
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2024-09-09 14:03:35 +0200

    stdlib: Improve strtoll, strtoull
    Add setting of errno on underflow, overflow and wrong base.
    Unify common code between strtoll and strtoull.
    Make code MISRA compliant.
    
    JIRA: RTOS-869

--- stdlib/strtoull.c
+++ stdlib/strtoull.c
@@ -17,6 +17,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <limits.h>
+#include <errno.h>
+
 
 intmax_t strtoimax(const char *nptr, char **endptr, int base)
 {
@@ -30,62 +32,82 @@ uintmax_t strtoumax(const char *nptr, char **endptr, int base)
 }
 
 
-unsigned long long int strtoull(const char *nptr, char **endptr, int base)
+static unsigned long long int strtoll_common(const char *nptr, char **endptr, int base, int isUnsigned)
 {
 	const char *s;
-	unsigned long long acc;
+	unsigned long long int acc, cutoff;
 	char c;
-	unsigned long long cutoff;
 	int neg, any, cutlim;
 
 	s = nptr;
 	do {
 		c = *s++;
-	} while (isspace((unsigned char)c));
+	} while (isspace(c));
 
 	if (c == '-') {
 		neg = 1;
 		c = *s++;
-	} else {
+	}
+	else {
 		neg = 0;
-		if (c == '+')
+		if (c == '+') {
 			c = *s++;
+		}
 	}
 
 	any = 0;
-	acc = 0;
+	acc = 0uLL;
 
-	if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
+	if (((base == 0) || (base == 16)) && (c == '0') && ((s[0] == 'x') || (s[0] == 'X')) && isxdigit(s[1])) {
 		c = s[1];
 		s += 2;
 		base = 16;
-		any = 1;
 	}
 
-	if (base == 0)
-		base = c == '0' ? 8 : 10;
+	if (base == 0) {
+		base = (c == '0') ? 8 : 10;
+	}
 
+	if ((base < 2) || (base > 36)) {
+		errno = EINVAL;
+		return 0uLL;
+	}
 
-	if (base < 2 || base > 36)
-		return acc;
+	if (isUnsigned != 0) {
+		cutoff = ULLONG_MAX;
+	}
+	else {
+		cutoff = (neg != 0) ? -LLONG_MIN : LLONG_MAX;
+	}
 
-	cutoff = ULONG_LONG_MAX / base;
-	cutlim = ULONG_LONG_MAX % base;
-	for ( ; ; c = *s++) {
-		if (c >= '0' && c <= '9')
+	cutlim = (int)(cutoff % base);
+	cutoff /= base;
+
+	for (;; c = *s++) {
+		if (c >= '0' && c <= '9') {
 			c -= '0';
-		else if (c >= 'A' && c <= 'Z')
+		}
+		else if (c >= 'A' && c <= 'Z') {
 			c -= 'A' - 10;
-		else if (c >= 'a' && c <= 'z')
+		}
+		else if (c >= 'a' && c <= 'z') {
 			c -= 'a' - 10;
-		else
+		}
+		else {
 			break;
+		}
 
-		if (c >= base)
+		if (c >= base) {
 			break;
+		}
 
-		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+		if (any < 0) {
+			continue;
+		}
+
+		if ((acc > cutoff) || ((acc == cutoff) && (c > cutlim))) {
 			any = -1;
+		}
 		else {
 			any = 1;
 			acc *= base;
@@ -93,99 +115,41 @@ unsigned long long int strtoull(const char *nptr, char **endptr, int base)
 		}
 	}
 
-	if (any < 0) {
-		acc = ULONG_LONG_MAX;
-	} else if (neg)
+	if (any == 0) {
+		errno = EINVAL;
+	}
+	else if (any < 0) {
+		errno = ERANGE;
+		if (isUnsigned != 0) {
+			acc = ULLONG_MAX;
+		}
+		else {
+			acc = (neg != 0) ? -LLONG_MIN : LLONG_MAX;
+		}
+	}
+	else if (neg != 0) {
 		acc = -acc;
+	}
 
-	if (endptr != NULL)
-		*endptr = (char *)(any ? s - 1 : nptr);
+	if (endptr != NULL) {
+		*endptr = (char *)((any != 0) ? (s - 1) : nptr);
+	}
 
-	return (acc);
+	return acc;
+}
+
+
+unsigned long long int strtoull(const char *nptr, char **endptr, int base)
+{
+	return strtoll_common(nptr, endptr, base, 1);
 }
 
 
 long long int strtoll(const char *nptr, char **endptr, int base)
 {
-	const char *s;
-	long long int acc, cutoff;
-	int c;
-	int neg, any, cutlim;
-
-	s = nptr;
-	do {
-		c = (unsigned char) *s++;
-	} while (isspace(c));
-
-	if (c == '-') {
-		neg = 1;
-		c = *s++;
-	} else {
-		neg = 0;
-		if (c == '+')
-			c = *s++;
-	}
-
-	if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
-		c = s[1];
-		s += 2;
-		base = 16;
-	}
-
-	if (base == 0)
-		base = c == '0' ? 8 : 10;
-
-	cutoff = neg ? LONG_LONG_MIN : LONG_LONG_MAX;
-	cutlim = (int)(cutoff % base);
-	cutoff /= base;
-	if (neg) {
-		if (cutlim > 0) {
-			cutlim -= base;
-			cutoff += 1;
-		}
-		cutlim = -cutlim;
-	}
-
-	for (acc = 0, any = 0;; c = (unsigned char) *s++) {
-		if (isdigit(c))
-			c -= '0';
-		else if (isalpha(c))
-			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
-		else
-			break;
-
-		if (c >= base)
-			break;
-
-		if (any < 0)
-			continue;
-
-		if (neg) {
-			if (acc < cutoff || (acc == cutoff && c > cutlim)) {
-				any = -1;
-				acc = LONG_LONG_MIN;
-			} else {
-				any = 1;
-				acc *= base;
-				acc -= c;
-			}
-		} else {
-			if (acc > cutoff || (acc == cutoff && c > cutlim)) {
-				any = -1;
-				acc = LONG_LONG_MAX;
-			} else {
-				any = 1;
-				acc *= base;
-				acc += c;
-			}
-		}
-	}
-
-	if (endptr != 0)
-		*endptr = (char *)(any ? s - 1 : nptr);
-
-	return (acc);
+	return (long long int)strtoll_common(nptr, endptr, base, 0);
 }
+
 
 long long int atoll(const char *str)
 {
