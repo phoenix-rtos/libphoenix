175c1e6b9d680066b5c3b390ac670804881c950a
adamdebek <adam.debek@phoenix-rtos.com>
2024-10-29 20:37:40 +0100

    libphoenix: make assert macro posix compliant
    POSIX states that assert macro should write fail information on stderr
    
    JIRA: RTOS-964

--- include/assert.h
+++ include/assert.h
@@ -28,7 +28,7 @@ extern "C" {
 
 #ifndef NDEBUG
 #define assert(__expr) \
-	((__expr) ? (void)0 : ({ printf("Assertion '%s' failed in file %s:%d, function %s.\n", #__expr, __FILE__, __LINE__, __func__); abort(); }))
+	((__expr) ? (void)0 : ({ fprintf(stderr, "Assertion '%s' failed in file %s:%d, function %s.\n", #__expr, __FILE__, __LINE__, __func__); abort(); }))
 #else
 
 
