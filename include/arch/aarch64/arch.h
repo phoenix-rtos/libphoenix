e36b848d741b782c160f832956f30a513bfb7057
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2025-01-09 16:55:04 +0100

    arch/aarch64: implement more functions with FPU instructions
    Implement fabs(), ceil(), floor(), round() and trunc() and their float
    counterparts with dedicated FPU instructions on AArch64.
    
    JIRA: RTOS-949

--- include/arch/aarch64/arch.h
+++ include/arch/aarch64/arch.h
@@ -24,11 +24,43 @@
 #define __MEMMOVE
 
 #ifndef __SOFTFP__
+/* clang-format off */
 #define __IEEE754_SQRT
 #define __ieee754_sqrt(x) ({ double a = (x); __asm__ ("fsqrt %d0, %d1" : "=w"(a) : "w"(a)); a; })
 
 #define __IEEE754_SQRTF
 #define __ieee754_sqrtf(x) ({ float a = (x); __asm__ ("fsqrt %s0, %s1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_FABS
+#define __ieee754_fabs(x) ({ double a = (x); __asm__ ("fabs %d0, %d1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_FABSF
+#define __ieee754_fabsf(x) ({ float a = (x); __asm__ ("fabs %s0, %s1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_CEIL
+#define __ieee754_ceil(x) ({ double a = (x); __asm__ ("frintp %d0, %d1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_CEILF
+#define __ieee754_ceilf(x) ({ float a = (x); __asm__ ("frintp %s0, %s1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_FLOOR
+#define __ieee754_floor(x) ({ double a = (x); __asm__ ("frintm %d0, %d1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_FLOORF
+#define __ieee754_floorf(x) ({ float a = (x); __asm__ ("frintm %s0, %s1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_ROUND
+#define __ieee754_round(x) ({ double a = (x); __asm__ ("frinta %d0, %d1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_ROUNDF
+#define __ieee754_roundf(x) ({ float a = (x); __asm__ ("frinta %s0, %s1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_TRUNC
+#define __ieee754_trunc(x) ({ double a = (x); __asm__ ("frintz %d0, %d1" : "=w"(a) : "w"(a)); a; })
+
+#define __IEEE754_TRUNCF
+#define __ieee754_truncf(x) ({ float a = (x); __asm__ ("frintz %s0, %s1" : "=w"(a) : "w"(a)); a; })
+/* clang-format on */
 #endif
 
 #define _PAGE_SIZE 0x1000
