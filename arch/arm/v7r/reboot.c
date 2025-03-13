9a48434e999a166e56826001b73768caa8c029c9
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2025-03-13 13:22:22 +0100

    arch: add support for armv7r architecture
    JIRA: RTOS-1004

---
+++ arch/arm/v7r/reboot.c
@@ -0,0 +1,52 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * reboot.c
+ *
+ * Copyright 2019, 2024 Phoenix Systems
+ * Author: Jan Sikorski, Jacek Maksymowicz
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <sys/reboot.h>
+#include <sys/platform.h>
+#if defined(__CPU_ZYNQMP)
+#include <phoenix/arch/armv7r/zynqmp/zynqmp.h>
+#else
+#error "Unsupported TARGET"
+#endif
+
+
+int reboot(int magic)
+{
+	platformctl_t pctl = {
+		.action = pctl_set,
+		.type = pctl_reboot,
+		.reboot = {
+			.magic = magic }
+	};
+
+	return platformctl(&pctl);
+}
+
+
+int reboot_reason(uint32_t *val)
+{
+	platformctl_t pctl = {
+		.action = pctl_get,
+		.type = pctl_reboot,
+	};
+
+	*val = 0;
+	if (platformctl(&pctl) < 0) {
+		return -1;
+	}
+
+	*val = pctl.reboot.reason;
+	return 0;
+}
---
+++ include/arch/armv7r/arch.h
@@ -0,0 +1,50 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * Architecture dependent part (arch/armv7r)
+ *
+ * Copyright 2017, 2024 Phoenix Systems
+ * Author: Pawel Pisarczyk, Lukasz Leczkowski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_ARCH_ARMV7R_ARCH_H_
+#define _LIBPHOENIX_ARCH_ARMV7R_ARCH_H_
+
+#define __ARCH_STDINT <arch/armv7r/stdint.h>
+#define __ARCH_LIMITS <arch/armv7r/limits.h>
+
+#define __MEMCPY
+#define __MEMCMP
+#define __MEMSET
+#define __STRLEN
+#define __STRNLEN
+#define __STRCMP
+#define __STRNCMP
+#define __STRCPY
+#define __STRNCPY
+#define __MEMMOVE
+
+
+#if __ARM_PCS_VFP || (__VFP_FP__ && !__SOFTFP__)
+#if __ARM_FP & 8
+#define __IEEE754_SQRT
+#define __ieee754_sqrt(x) ({ double a = (x); __asm__ volatile ("vsqrt.f64 %P0, %P1" : "=w"(a) : "w"(a)); a; })
+#endif
+
+#define __IEEE754_SQRTF
+#define __ieee754_sqrtf(x) ({ float a = (x); __asm__ volatile ("vsqrt.f32 %0, %1" : "=t"(a) : "t"(a)); a; })
+#endif
+
+
+#define _PAGE_SIZE 0x1000
+#define SIZE_PAGE  _Pragma("GCC warning \"'SIZE_PAGE' is deprecated. Use _PAGE_SIZE from arch.h or PAGE_SIZE from limits.h (POSIX only)\"") _PAGE_SIZE
+
+#define __LIBPHOENIX_ARCH_TLS_SUPPORTED
+
+#endif
