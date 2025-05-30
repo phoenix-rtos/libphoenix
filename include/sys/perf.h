70f54dbeb2af73ece0d484c000e30253789bd2c4
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-05-30 13:29:59 +0200

    !perf: generalize perf syscalls
    Previous perf semantics are preserved by passing `perf_mode_threads`
    
    JIRA: RTOS-1057

---
+++ include/sys/perf.h
@@ -0,0 +1,38 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * sys/perf
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Adam Greloch
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBPHOENIX_SYS_PERF_H_
+#define _LIBPHOENIX_SYS_PERF_H_
+
+#include <phoenix/perf.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern int perf_start(perf_mode_t mode, unsigned pid);
+
+
+extern int perf_read(perf_mode_t mode, void *buffer, size_t bufsz);
+
+
+extern int perf_finish(perf_mode_t mode);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
