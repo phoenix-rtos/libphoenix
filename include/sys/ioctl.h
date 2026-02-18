a7d110c08494858c74fe944bdd0d866157bb9050
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-02-18 11:22:22 +0100

    !include/sys/ioctl: include phoenix/ioctl.h for common IOC* defines
    IOC* defines transferred to kernel to reduce code duplication
    
    JIRA: RTOS-1205

--- include/sys/ioctl.h
+++ include/sys/ioctl.h
@@ -21,6 +21,8 @@
 #include <sys/msg.h>
 #include <sys/types.h>
 
+#include <phoenix/ioctl.h>
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,30 +30,15 @@ extern "C" {
 
 
 /* convenience macro to get sub-pointer `field` in a packed ioctl structure that is read-only */
-#define IOC_GET_PTR_FIELD(request, subptr, val, field) ioctl_getPointerField(request, subptr, val, offsetof(typeof(*val), field))
+#define IOC_NESTED_GET_PTR_FIELD(request, subptr, val, field) ioctl_getPointerField(request, subptr, val, offsetof(typeof(*val), field))
 
 
-#define IOCPARM_MASK   0x1fff
-#define IOCPARM_LEN(x) (((x) >> 16) & IOCPARM_MASK)
-#define IOCBASECMD(x)  ((x) & ~(IOCPARM_MASK << 16))
-#define IOCGROUP(x)    (((x) >> 8) & 0xff)
-
-
-#define IOC_VOID    0x00000000
-#define IOC_NESTED  0x20000000
-#define IOC_OUT     0x40000000
-#define IOC_IN      0x80000000
-#define IOC_INOUT   (IOC_IN | IOC_OUT)
-#define IOC_DIRMASK 0xe0000000
-
-
-#define _IOC(inout, group, num, len) ((unsigned long)(inout | ((len & IOCPARM_MASK) << 16) | ((group) << 8) | (num)))
-#define _IO(g, n)                    _IOC(IOC_VOID, (g), (n), 0)
-#define _IOV(g, n, t)                _IOC(IOC_VOID, (g), (n), sizeof(t)) /* IOW with passing by value */
-#define _IOR(g, n, t)                _IOC(IOC_OUT, (g), (n), sizeof(t))
-#define _IOW(g, n, t)                _IOC(IOC_IN, (g), (n), sizeof(t))
-#define _IOWR(g, n, t)               _IOC(IOC_INOUT, (g), (n), sizeof(t))
-#define _IOC_NESTED(inout, g, n, t)  (IOC_NESTED | _IOC(inout, g, n, sizeof(t)))
+#define _IO(g, n)                   _IOC(IOC_VOID, (g), (n), 0)
+#define _IOV(g, n, t)               _IOC(IOC_VOID, (g), (n), sizeof(t)) /* IOW with passing by value */
+#define _IOR(g, n, t)               _IOC(IOC_OUT, (g), (n), sizeof(t))
+#define _IOW(g, n, t)               _IOC(IOC_IN, (g), (n), sizeof(t))
+#define _IOWR(g, n, t)              _IOC(IOC_INOUT, (g), (n), sizeof(t))
+#define _IOC_NESTED(inout, g, n, t) _IOC((inout) | IOC_NESTED, (g), (n), sizeof(t)) /* passed structure is nested */
 
 
 #define TIOCGPTN   _IOR('T', 0x30, unsigned int)
