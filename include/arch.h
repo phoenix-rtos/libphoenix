41c649eaa6baa3bafa80128936e2d0e749dbeb86
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-05-27 15:26:19 +0200

    include/arch: include correct arch.h for armv7r
    YT: RTOS-1340

--- include/arch.h
+++ include/arch.h
@@ -2,7 +2,9 @@
 #include <arch/ia32/arch.h>
 #elif defined(__ARM_ARCH_6M__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
 #include <arch/armv7m/arch.h>
-#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_8A__) || defined(__ARM_ARCH_7__)
+#elif defined(__ARM_ARCH_7R__)
+#include <arch/armv7r/arch.h>
+#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_8A__) || defined(__ARM_ARCH_7__)
 #include <arch/armv7a/arch.h>
 #elif defined(__ARM_ARCH_4T__) || defined(__ARM_ARCH_5TE__) /* not currently supported, map to 7M for libgcc to compile */
 #include <arch/armv7m/arch.h>
