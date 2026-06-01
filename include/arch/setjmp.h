e718951931bb13cb4305c74cdf37c4226be9e40b
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-06-01 13:19:55 +0200

    include/arch/setjmp: include correct arch.h for armv7r
    YT: RTOS-1340

--- include/arch/setjmp.h
+++ include/arch/setjmp.h
@@ -17,7 +17,9 @@
 #include <arch/ia32/setjmp.h>
 #elif defined(__ARM_ARCH_6M__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__) || defined(__ARM_ARCH_8M_BASE__) || defined(__ARM_ARCH_8M_MAIN__)
 #include <arch/armv7m/setjmp.h>
-#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_8A__) || defined(__ARM_ARCH_7__)
+#elif defined(__ARM_ARCH_7R__)
+#include <arch/armv7r/setjmp.h>
+#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_8A__) || defined(__ARM_ARCH_7__)
 #include <arch/armv7a/setjmp.h>
 #elif defined(__ARM_ARCH_4T__) || defined(__ARM_ARCH_5TE__) /* not currently supported, map to 7M for libgcc to compile */
 #include <arch/armv7m/setjmp.h>
