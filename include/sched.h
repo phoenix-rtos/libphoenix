2741ecb733288fa4462a06c67070c1c5bff86c4b
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-06-17 12:39:17 +0200

    include/sched.h: use sched defines from the kernel header
    TASK: RTOS-1353

--- include/sched.h
+++ include/sched.h
@@ -18,6 +18,7 @@
 
 
 #include <errno.h>
+#include <phoenix/sched.h>
 
 
 #ifdef __cplusplus
@@ -25,23 +26,18 @@ extern "C" {
 #endif
 
 
-#define SCHED_FIFO 0
-#define SCHED_RR 1
-#define SCHED_OTHER 2
-
-
 struct sched_param {
 	int sched_priority;
 };
 
 
-extern int sched_yield(void);
+int sched_yield(void);
 
 
-extern int sched_get_priority_max(int policy);
+int sched_get_priority_max(int policy);
 
 
-extern int sched_get_priority_min(int policy);
+int sched_get_priority_min(int policy);
 
 
 #ifdef __cplusplus
