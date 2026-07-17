c793265270f67b8845e26277fcc118b50466af74
Mikołaj Matałowski <mikolaj.matalowski@phoenix-rtos.com>
2026-07-17 17:25:38 +0200

    libm: Move math library implementation, split implementation to seperate files
    YT: RTOS-1132

---
+++ libm/phoenix/mathd/acosd.c
@@ -0,0 +1,51 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * cos, sin, tan, acos, asin, atan
+ *
+ * Copyright 2017, 2018, 2026 Phoenix Systems
+ * Author: Aleksander Kaminski, Jakub Smolaga
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+ 
+#include <math.h>
+#include <errno.h>
+
+
+/* Calculates value of arc cosine using secant method */
+double acos(double x)
+{
+	double xa = 0, xb = M_PI, ya, yb, t;
+	int i;
+
+	if (isnan(x) != 0) {
+		return NAN;
+	}
+
+	if ((x > 1.0) || (x < -1.0)) {
+		errno = EDOM;
+		return NAN;
+	}
+
+	for (i = 0; i < 16; ++i) {
+		ya = cos(xa) - x;
+		yb = cos(xb) - x;
+
+		t = ya - yb;
+
+		if (t == 0.0) {
+			break;
+		}
+
+		t = ((ya * xb) - (yb * xa)) / t;
+		xa = xb;
+		xb = t;
+	}
+
+	return xb;
+}
---
+++ libm/phoenix/mathd/tanhd.c
@@ -0,0 +1,34 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * hyberbolic functions
+ *
+ * Copyright 2018 Phoenix Systems
+ * Author: Aleksander Kaminski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+ 
+#include <math.h>
+
+
+double tanh(double x)
+{
+	if (isnan(x) != 0) {
+		return NAN;
+	}
+
+	if (x == INFINITY) {
+		return 1.0;
+	}
+	else if (x == -INFINITY) {
+		return -1.0;
+	}
+
+	/* cosh is never equal to zero */
+	return (sinh(x) / cosh(x));
+}
