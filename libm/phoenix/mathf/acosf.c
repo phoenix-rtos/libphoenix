d6f767eb960978aad22327cb251f2a0071c283f3
Mikołaj Matałowski <mikolaj.matalowski@phoenix-rtos.com>
2026-07-17 17:25:38 +0200

    libm: Move math library implementation, split implementation to seperate files
    YT: RTOS-1132

---
+++ libm/phoenix/mathf/acosf.c
@@ -0,0 +1,22 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix
+ *
+ * cos, sin, tan, acos, asin, atan
+ *
+ * Copyright 2017, 2018, 2026 Phoenix Systems
+ * Author: Aleksander Kaminski, Jakub Smolaga
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+ 
+#include <math.h>
+
+
+float acosf(float x)
+{
+	return (float)acos((double)x);
+}
