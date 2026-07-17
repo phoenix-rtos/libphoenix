5769bc930ac307545e44b27b8a3d39b829dca598
Mikołaj Matałowski <mikolaj.matalowski@phoenix-rtos.com>
2026-07-17 17:28:48 +0200

    libm: Add architecture specific math implementation
    YT: RTOS-1132

---
+++ libm/arch/aarch64/sqrtd.c
@@ -0,0 +1,37 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix/libm
+ *
+ * Architecture dependent part (arch/aarch64)
+ *
+ * Copyright 2017, 2018, 2026 Phoenix Systems
+ * Author: Pawel Pisarczyk, Aleksander Kaminski, Michal Lach
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+ 
+#include <math.h>
+#include <errno.h>
+
+
+double sqrt(double x)
+{
+	if (isnan(x) != 0) {
+		return NAN;
+	}
+
+	if (x < 0.0) {
+		errno = EDOM;
+		return NAN;
+	}
+
+	if ((x == 0.0) || (x == INFINITY)) {
+		return x;
+	}
+
+	__asm__ volatile("fsqrt %d0, %d1" : "=w"(x) : "w"(x));
+	return x;
+}
---
+++ libm/arch/riscv64/sqrtd.c
@@ -0,0 +1,39 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libphoenix/libm
+ *
+ * Architecture dependent part (RISCV64)
+ *
+ * Copyright 2018, 2026 Phoenix Systems
+ * Author: Pawel Pisarczyk, Michal Lach
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <math.h>
+#include <errno.h>
+
+double sqrt(double x)
+{
+	if (isnan(x) != 0) {
+		return NAN;
+	}
+
+	if (x < 0.0) {
+		errno = EDOM;
+		return NAN;
+	}
+
+	if ((x == 0.0) || (x == INFINITY)) {
+		return x;
+	}
+
+	/* clang-format off */
+	__asm__ volatile ("fsqrt.d %0, %1" : "=f"(x) : "f"(x));
+	/* clang-format on */
+
+	return x;
+}
