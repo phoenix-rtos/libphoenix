0e62403608038f20487614fcd792bdc74f89ad1c
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-08-06 13:26:00 +0200

    unistd/pause: implement pause with sigsuspend
    YT: RTOS-1417

--- unistd/pause.c
+++ unistd/pause.c
@@ -14,42 +14,12 @@
  */
 
 #include <unistd.h>
-#include <errno.h>
-#include <sys/threads.h>
+#include <signal.h>
 
 
 int pause(void)
 {
-	handle_t cond, lock;
-	int err;
-
-	err = condCreate(&cond);
-	if (err < 0) {
-		return SET_ERRNO(err);
-	}
-
-	err = mutexCreate(&lock);
-	if (err < 0) {
-		resourceDestroy(cond);
-		return SET_ERRNO(err);
-	}
-
-	err = phMutexLock(lock);
-	if (err >= 0) {
-		/* Sleep on a private cond - only signal can wake us up */
-		while (err >= 0) {
-			err = phCondWait(cond, lock, 0);
-		}
-
-		if (err != -EINTR) {
-			/* On EINTR phCondWait does not relock the lock.
-			 * Unlock it manually on other cases */
-			(void)mutexUnlock(lock);
-		}
-	}
-
-	resourceDestroy(cond);
-	resourceDestroy(lock);
-
-	return SET_ERRNO(err);
+	sigset_t mask;
+	(void)sigprocmask(SIG_BLOCK, NULL, &mask);
+	return sigsuspend(&mask);
 }
